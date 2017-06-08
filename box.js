3647b69aaa25cdf2fe9215b08761882f7cfefb23
王成飞 <764297968@qq.com>
2017-06-08 18:29:56 +0800

    confirm

---
+++ box.js
@@ -0,0 +1,12 @@
+﻿var box = new Object();
+box.confirm=function(message,callback)
+{
+    $(".confirm").show();
+    $(".con").unbind().click(function () {
+        callback.call(this, true);
+    })
+    $(".can").unbind().click(function () {
+        $(".confirm").hide();
+        callback.call(this, false);
+    })
+}
